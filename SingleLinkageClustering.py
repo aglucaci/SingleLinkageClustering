4a69030905ca2871bc35648df84c07bebb1de65a
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-01-07 11:24:15 -0500

    Update SingleLinkageClustering.py

--- SingleLinkageClustering.py
+++ SingleLinkageClustering.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-
 @author: alexander lucaci
+@Temple University
 """
 # =============================================================================
 # Imports
